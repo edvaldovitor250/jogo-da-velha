71c85edef51906dcd32c730eb5a618c19360384c
Edvaldo Vitor De Castro Souza <edvaldovitor250@gmail.com>
2024-03-10 14:01:55 -0300

    add: function check game

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ let square = {
 
 let player = '';
 let warning = '';
-let plating = false;
+let playing = false;
 
 // Events
 
@@ -50,6 +50,8 @@ function renderSquare() {
         let item = document.querySelector(`div[data-item=${i}]`);
         item.textContent = square[i];
     }
+
+    checkGame();
 }
 function renderInfo() {
     document.querySelector(`.vez`).innerHTML = player
@@ -59,4 +61,49 @@ function renderInfo() {
 function togglePlayer() {
     player = (player === 'x')? 'o' : 'x';
     renderInfo();
+}
+
+function checkGame() {
+    if (checkWinnerFor('x')) {
+        warning = 'O "x" venceu'
+        playing = false;
+        
+    } else if (checkWinnerFor('o')) {
+        warning = 'O "o" venceu';
+        playing = false;
+    } else if (isFull()){
+        warning = 'Empate';
+        playing = false;
+    }
+}
+
+function checkGame(player) {
+    let pos = [
+        'a1,a2,a3',
+        'b1,b2,b3',
+        'c1,c2,c3',
+
+        'a1,b1,c1',
+        'a2,b2,c2',
+        'a3,b3,c3',
+
+        'a1,b2,c3',
+        'a3,b2,c1',
+
+    ];
+
+    for (let w in pos){
+        let pArray = pos[w].split(',');
+      let hasWon =  pArray.every(option => square[option] === player)
+      if (hasWon) {
+        return true;
+      } 
+    }
+
+    return false;
+
+}
+
+function isFull() {
+
 }
