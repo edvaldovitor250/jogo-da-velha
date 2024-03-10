007b678dc5482120abaec277594f7dcaf3402d50
Edvaldo Vitor De Castro Souza <edvaldovitor250@gmail.com>
2024-03-10 10:50:18 -0300

    add: reset function

--- script.js
+++ script.js
@@ -11,4 +11,36 @@ let plating = false;
 
 // Events
 
+document.querySelector('.reset').addEventListener('click',reset);
+
+
 // Funcitons
+
+function reset() {
+    warning = '';
+
+    let random = Math.floor(Math.random() *2);
+    player = (random === 0 ) ? 'x' : 'o';
+    
+   for(let i in square){
+    square[i] = '';
+   }
+
+   plating = true;
+
+   
+   renderSquare();
+   renderInfo();
+}
+
+function renderSquare() {
+    for( let i in square ) {
+       let item = document.querySelector(`div[data-item=${i}]`);
+       item.textContent = square[i];
+    }
+
+}
+function renderInfo() {
+    document.querySelector(`.vez`).innerHTML = player
+    document.querySelector(`.warning`).innerHTML = warning
+}
